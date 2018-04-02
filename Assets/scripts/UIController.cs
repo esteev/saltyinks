a1faa87193d027d9b42ad1c2ad497c725111671f
Suryank <suryank@live.com>
2018-04-02 23:44:03 +0530

    facebook login working

---
+++ Assets/scripts/UIController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+public class UIController : MonoBehaviour {
+
+	void Start () {
+		if (!FB.IsInitialized) {
+			// Initialize the Facebook SDK
+			FB.Init(InitCallback, OnHideUnity);
+		} else {
+			// Already initialized, signal an app activation App Event
+			FB.ActivateApp();
+		}
+	}
+
+	private void AuthCallback (ILoginResult result) {
+		if (FB.IsLoggedIn) {
+			// AccessToken class will have session details
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
+			// Print current access token's granted permissions
+			foreach (string perm in aToken.Permissions) {
+				Debug.Log(perm);
+			}
+		} else {
+			Debug.Log("User cancelled login");
+		}
+	}
+
+	private void InitCallback ()
+	{
+		if (FB.IsInitialized) {
+			// Signal an app activation App Event
+			FB.ActivateApp();
+			// Continue with Facebook SDK
+			// ...
+		} else {
+			Debug.Log("Failed to Initialize the Facebook SDK");
+		}
+	}
+
+	private void OnHideUnity (bool isGameShown)
+	{
+		if (!isGameShown) {
+			// Pause the game - we will need to hide
+			Time.timeScale = 0;
+		} else {
+			// Resume the game - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
+
+	public void fbLogin()
+	{
+		if (kaChow.Instance.fbLogin == 0) {
+			var perms = new List<string> (){ "public_profile", "email" };
+			FB.LogInWithReadPermissions (perms, AuthCallback);
+		}
+	}
+
+	public void googleLogin()
+	{
+		
+	}
+
+}
---
+++ Assets/scripts/kaChow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kaChow : MonoBehaviour {
+
+	private static kaChow instance;
+	public static kaChow Instance{get{ return instance; }}
+
+	public int fbLogin = 0;
+
+	void Start () {
+		instance = this;
+//		DontDestroyOnLoad (gameObject);
+
+		if (PlayerPrefs.HasKey ("init")) 
+		{
+			fbLogin = PlayerPrefs.GetInt ("fbLogin");
+		} 
+		else 
+		{
+			PlayerPrefs.SetInt ("init", 1);
+			Save ();
+		}
+	}
+
+	public void Save()
+	{
+		PlayerPrefs.SetInt ("fbLogin", fbLogin);
+	}
+
+}

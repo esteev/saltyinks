2285a639adfbe483363c34fcb9d6bfaa9a5645ba
Suryank <suryank@live.com>
2018-04-03 12:25:41 +0530

    init screen anim completed

--- Assets/scripts/UIController.cs
+++ Assets/scripts/UIController.cs
@@ -5,7 +5,17 @@ using Facebook.Unity;
 
 public class UIController : MonoBehaviour {
 
-	void Start () {
+    private float invokeCallRate = 0.05f;
+    private float initLoginSpeed = 150f;
+
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
+
+    public GameObject animatables, dotdot;
+    private int animState = 1;
+
+    void Start () {
 		if (!FB.IsInitialized) {
 			// Initialize the Facebook SDK
 			FB.Init(InitCallback, OnHideUnity);
@@ -13,9 +23,134 @@ public class UIController : MonoBehaviour {
 			// Already initialized, signal an app activation App Event
 			FB.ActivateApp();
 		}
-	}
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+    }
 
-	private void AuthCallback (ILoginResult result) {
+    void Update()
+    {
+    /*    if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   //Right swipe
+                            Debug.Log("Right Swipe");
+
+                            right();
+                        }
+                        else
+                        {   //Left swipe
+                            Debug.Log("Left Swipe");
+                            left();
+                        }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            Debug.Log("Up Swipe");
+                        }
+                        else
+                        {   //Down swipe
+                            Debug.Log("Down Swipe");
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                }
+            }
+        }
+        */
+    }
+
+    Vector3[] target = { Vector3.zero, new Vector3(-600f, 0f, 0f), new Vector3(-1200f, 0f, 0f) };
+
+    void getToPos()
+    {
+        if (animatables.GetComponent<Transform>().localPosition != target[animState-1])
+        {
+            animatables.GetComponent<Transform>().localPosition = Vector3.MoveTowards(animatables.GetComponent<Transform>().localPosition, target[animState - 1], initLoginSpeed);
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
+    public void righty()
+    {
+        print(animState);
+   //     enableAnimation();
+        switch (animState)
+        {
+            case 1:
+                animState = 2;
+                InvokeRepeating("getToPos", invokeCallRate, invokeCallRate);
+            //    animatables.GetComponent<Animation>().Play("12");
+                dotdot.GetComponent<Animation>().Play("dot12");
+                break;
+            case 2:
+                animState = 3;
+                InvokeRepeating("getToPos", invokeCallRate, invokeCallRate);
+                dotdot.GetComponent<Animation>().Play("dot23");
+                break;
+        }
+    //    disableAnimation();
+    }
+
+    public void lefty()
+    {
+        print(animState);
+        //     enableAnimation();
+        switch (animState)
+        {
+            case 3:
+                animState = 2;
+                InvokeRepeating("getToPos", invokeCallRate, invokeCallRate);
+                dotdot.GetComponent<Animation>().Play("dot32");
+                break;
+            case 2:
+                animState = 1;
+                InvokeRepeating("getToPos", invokeCallRate, invokeCallRate);
+                dotdot.GetComponent<Animation>().Play("dot21");
+                break;
+        }
+    //    disableAnimation();
+    }
+
+    void enableAnimation()
+    {
+        animatables.GetComponent<Animation>().enabled = true;
+        dotdot.GetComponent<Animation>().enabled = true;
+    }
+
+    void disableAnimation()
+    {
+        animatables.GetComponent<Animation>().enabled = false;
+        dotdot.GetComponent<Animation>().enabled = false;
+    }
+
+    private void AuthCallback (ILoginResult result) {
 		if (FB.IsLoggedIn) {
 			// AccessToken class will have session details
 			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
